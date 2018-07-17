15da0cda3f7cb5ae17d8846fe55a8fe358272978
Ishtiaq Mahmood Amin <imamin@ael.net>
2018-07-17 20:26:06 +0600

    After Finishing: 089 MapStateToProps with Connect

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -3,13 +3,20 @@ import { connect } from 'react-redux';
 import { View, Text, } from 'react-native';
 
 class LibraryList extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    );
-  }
+    render() {
+        console.log(this.props);
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        );
+    }
 }
 
-export default connect()(LibraryList);
+const mapStateToProps = state => {
+    return {
+        libraries: state.libraries
+    };
+};
+
+export default connect(mapStateToProps)(LibraryList);
