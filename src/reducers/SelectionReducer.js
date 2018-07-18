8d10b9c4dc08f2c7b9a82e5172eff64a3480321d
Ishtiaq Mahmood Amin <imamin@ael.net>
2018-07-18 16:49:45 +0600

    After Finishing: 099 Rules of Reducers

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,6 +1,13 @@
-export default (state, action) => {
-    // default value cannot be 'undefined'
-    console.log(action);
-    return null;
+export default (state = null 
+    /* Reducers should never return 'undefined',
+     thats why a default value 'null' is assigned to 'state'*/
+     , action) => {
+    switch (action.type) {
+        case 'select_library':
+            return action.payload;
+        default:
+            return state;
+
+    }
 };
 
